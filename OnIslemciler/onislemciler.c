866cd380e4d27b4533ebfdba0e4cf0ee628b54f0
ogunbrnc <48843277+ogunbrnc@users.noreply.github.com>
2021-03-23 21:55:17 +0300

    Onislemciler konusu ve detaylari anlatildi

--- OnIslemciler/onislemciler.c
+++ OnIslemciler/onislemciler.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+#define TURKCE
+#define INGILIZCE
+
+	
+
+
+int main(void){
+	#undef INGILIZCE
+	
+	#if defined(TURKCE)
+		printf("Turkce tanimlanmistir.\n");
+	#elif defined(INGILIZCE)
+		printf("Ingilizce tanimlanmistir.\n");
+	#else
+		printf("Turkce-ingilizce tanimlanmamistir.\n");
+	#endif
+	
+	
+	
+	
+	
+	return 0;
+}
